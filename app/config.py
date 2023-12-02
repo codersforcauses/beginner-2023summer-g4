5e50daa12b0b7c356126832d47479ef44a643880
torry2 <mail@torrytw.ooo>
2023-12-02 13:20:51 +0800

    add dev config

--- app/config.py
+++ app/config.py
@@ -3,6 +3,7 @@
 DEBUG = True
 uvicorn_LOGGING = "info" # uvicorn.config.LOGGING_CONFIG
 
+DEVELOPMENT = True # enable uvicorn reload
 uvicorn_HOST = "127.0.0.1" ; uvicorn_PORT = 5000
 
 # DATABASE = False
