1ce70bee5d9b968ef6514c7aeb0f3f8b303211de
torry2 <mail@torrytw.ooo>
2024-01-06 15:54:38 +0800

    please

--- app/backend/api.py
+++ app/backend/api.py
@@ -29,11 +29,6 @@ async def complete(request: Request):
     response = {"tmp": "response"} # change for nhigh score n shit later
     return JSONResponse(content=response) 
 
-'''
-idk = {"usern": "name", "totalscore":"0"}
-
-'''
-
 @router.post("/submit") # rename to slash round later
 async def rround(request: Request):
     data = await request.json()
@@ -44,10 +39,6 @@ async def rround(request: Request):
         points = city_points(data)
         response = {"score": points}
         return JSONResponse(content=response)
-        else:
-            points = city_points(data)
-            response = {"score": points}
-            return JSONResponse(content=response)
 
     elif data["game_mode"] == "sleuth":
         print("HANDLE SLUETH")
