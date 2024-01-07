7654773c72ff832e34017cafc58e5916e20f391b
torry2 <mail@torrytw.ooo>
2024-01-07 13:58:49 +0800

    config stuff

--- app/config.py
+++ app/config.py
@@ -1,13 +1,29 @@
 # config.py
 
-DEBUG = True
-uvicorn_LOGGING = "info" # uvicorn.config.LOGGING_CONFIG
+# Perth PinPoint Configuration
 
-DEVELOPMENT = True # enable uvicorn reload
-uvicorn_HOST = "127.0.0.1" ; uvicorn_PORT = 5000
+# Run Silently, with or without a Banner
+SILENT = False ; BANNER = True
 
-#HEALTH = True # check api alive / do this later
-DATABASE = True # rebuild database
+# Production
+# Enabled, Disabled if "DEV" True
 
+##### PROD OPTIONS #####
+uvicorn_LOGGING = "error"
+uvicorn_HOST = "127.0.0.1"
+uvicorn_PORT = 5000
+##### PROD OPTIONS #####
+
+# Development
+DEV = True
+# WARNING: Please check the DEV_uvicorn_HOST/PORT, this is not reflected in the {pp_banner}!
+
+##### DEV OPTIONS #####
+DEV_DEBUG = True ; DEV_uvicorn_LOGGING = "trace" # uvicorn.config.LOGGING_CONFIG // 'critical', 'error', 'warning', 'info', 'debug', 'trace'
+DEV_RELOAD = True
+DEV_uvicorn_HOST = "127.0.0.1" ; DEV_uvicorn_PORT = 5000
+#DEV_HEALTH = True # to add
+DEV_DATABASE = True # rebuild database
+##### DEV OPTIONS #####
 
 
--- app/init.py
+++ app/init.py
@@ -7,21 +7,39 @@ from core.constants import *
 
 if __name__ == "__main__":
 
-    if DEBUG:
-        print(f"[+] DEBUG: True, Debugging (log) enabled.")
+    if DEV:
+        print(f"[+] Development Mode Enabled")
 
-    log(f"[+] Initialising Application")
+        if DEV_DEBUG:
+            print(f"[+] DEBUG: True, Debugging (log) enabled.")
 
-    if DATABASE:
-        log(f"[+] Rebuilding Database")
-        rebuild(path_db)
+        log(f"[+] Initialising Application")
+
+        uvicorn_LOGGING = DEV_uvicorn_LOGGING
+        RELOAD = DEV_RELOAD
+        uvicorn_HOST = DEV_uvicorn_HOST
+        uvicorn_PORT = DEV_uvicorn_PORT
+        log(f"[+] Configured Development Options")
+
+        if DEV_DATABASE:
+            log(f"[+] Rebuilding Database")
+            rebuild(path_db)
+
+    if SILENT:
+        uvicorn_LOGGING = "critical"
     
+    # inherit options automatically (prod config)
+    RELOAD = False
+
+    if BANNER:
+        print(f"{pp_banner}")
+
     uvicorn.run(
         "core.main:app",
         host=uvicorn_HOST,
         port=uvicorn_PORT,
         log_level=uvicorn_LOGGING,
-        reload=DEVELOPMENT
+        reload=RELOAD
         )
 
 '''
