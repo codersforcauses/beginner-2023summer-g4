f898f656a5dab6ed6a3879298b4a784e021c564f
torry2 <mail@torrytw.ooo>
2024-01-11 09:31:43 +0800

    timestamped logs

--- app/config.py
+++ app/config.py
@@ -12,6 +12,8 @@ SILENT = False ; BANNER = True
 uvicorn_LOGGING = "error"
 uvicorn_HOST = "0.0.0.0"
 uvicorn_PORT = 5000
+uvicorn_WORKERS = 4
+uvicorn_ACCESSLOG = True
 ##### PROD OPTIONS #####
 
 # Development
@@ -20,8 +22,9 @@ DEV = True
 
 ##### DEV OPTIONS #####
 DEV_DEBUG = True ; DEV_uvicorn_LOGGING = "trace" # uvicorn.config.LOGGING_CONFIG // 'critical', 'error', 'warning', 'info', 'debug', 'trace'
-DEV_RELOAD = True
+DEV_RELOAD = True ; DEV_WORKERS = 1
 DEV_uvicorn_HOST = "127.0.0.1" ; DEV_uvicorn_PORT = 5000
+DEV_ACCESSLOG = True
 #DEV_HEALTH = True #todo
 DEV_DATABASE = True # rebuild database
 ##### DEV OPTIONS #####
